969103e2d698b280512e6755d09ad1ecc95d3994
saumya singh <saumyasingh3234@gmail.com>
2025-12-21 10:21:08 +0530

    files added

---
+++ dec19ques1.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/P1_175?tab=statement
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    if (x > y) {
+        cout << "no";
+    } else {
+        cout << "yes";
+    }
+}
