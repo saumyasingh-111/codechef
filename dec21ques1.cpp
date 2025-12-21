877b0ad3bb1c61fc8b4e13f3a01ba68f9783e78a
saumya singh <saumyasingh3234@gmail.com>
2025-12-21 10:27:31 +0530

    ques 1

---
+++ dec21ques1.cpp
@@ -0,0 +1,15 @@
+// https://www.codechef.com/problems/SQUIDBANK
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+int n,k;
+cin>>n>>k;
+int left=n-k;
+int prize=left*10000;
+cout<<prize;
+}
