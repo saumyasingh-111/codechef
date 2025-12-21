ad5283e87d5ce01aa66173b5b0c9f544ed24a0c2
saumya singh <saumyasingh3234@gmail.com>
2025-12-21 10:55:26 +0530

    ques 5

---
+++ dec21ques5.cpp
@@ -0,0 +1,12 @@
+// https://www.codechef.com/problems/REMMON
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+int main() {
+	// your code goes here
+int n,a,b;
+cin>>n>>a>>b;
+cout<<n-(a*b);
+}
