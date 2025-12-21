ef73d4363bf1306741d3dae196476dd48b06cc4b
saumya singh <saumyasingh3234@gmail.com>
2025-12-21 10:39:26 +0530

    ques 3

---
+++ dec21ques3.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/AICOM
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x;
+    cin >> x;
+    if (x <= 60) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+}
