e4576421404860c7e394b043adb629ba5f229aa0
saumya singh <saumyasingh3234@gmail.com>
2025-12-27 13:09:06 +0530

    SKONG

---
+++ dec27ques2.cpp
@@ -0,0 +1,14 @@
+// https://www.codechef.com/problems/SKONG
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x, h;
+    cin >> x >> h;
+    cout << (x - 4) * 24 + h;
+
+}
