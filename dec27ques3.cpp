4d4f98616197b28931e176602232ff9ee95a285f
saumya singh <saumyasingh3234@gmail.com>
2025-12-27 14:00:17 +0530

    NOTECNT

---
+++ dec27ques3.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/NOTECNT
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int a, b;
+    cin >> a >> b;
+    cout << (a * b * 50 * 2);
+}
