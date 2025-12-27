3b4165850533311c9caf12160a54efb9ce656b8e
saumya singh <saumyasingh3234@gmail.com>
2025-12-27 14:03:05 +0530

    P1209

---
+++ dec27ques4.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/P1209
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int r;
+    cin >> r;
+    if (r <= 4) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+}
