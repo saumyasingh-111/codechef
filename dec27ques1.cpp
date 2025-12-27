cbbdf410d4921822ce135a19f98131184ba83de8
saumya singh <saumyasingh3234@gmail.com>
2025-12-27 13:00:20 +0530

    HJJ

---
+++ dec27ques1.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/HJJ
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x;
+    cin >> x;
+    cout << x + 20;
+}
