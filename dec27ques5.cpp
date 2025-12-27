d8a11c0eff4b1a0cceef87016ce32d15fc4c1ff8
saumya singh <saumyasingh3234@gmail.com>
2025-12-27 14:10:48 +0530

    PZSPLIT

---
+++ dec27ques5.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/PZSPLIT
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int n;
+    cin >> n;
+    if (n % 2 == 0) {
+        cout << 1;
+    } else {
+        cout << 2;
+    }
+}
