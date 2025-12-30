5fc3096bd81af12f61aeb4e52dfc49231e8d354f
saumya singh <saumyasingh3234@gmail.com>
2025-12-30 16:31:43 +0530

    untracked file

---
+++ dec30ques1.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/BAKECAKE1
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int n, m;
+    cin >> n >> m;
+    cout << (m * 50 - n * 30);
+}
