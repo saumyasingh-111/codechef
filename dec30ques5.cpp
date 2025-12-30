dfe46176ec6297a374b21cb968e7a0560a695ed4
saumya singh <saumyasingh3234@gmail.com>
2025-12-30 16:30:57 +0530

    dec30ques5

---
+++ dec30ques5.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/SPRCLN
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x, y;
+    cin >> x >> y;
+    cout << (x * 30 + y * 60);
+}
