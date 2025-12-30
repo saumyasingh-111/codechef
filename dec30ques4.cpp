0d9df8f7b2bc56fa7d1d1d8ce190bee57194de21
saumya singh <saumyasingh3234@gmail.com>
2025-12-30 16:28:55 +0530

    dec30ques4

---
+++ dec30ques4.cpp
@@ -0,0 +1,17 @@
+// https://www.codechef.com/problems/P1169
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x;
+    cin >> x;
+    if (x >= 10) {
+        cout << "yes";
+    } else {
+        cout << "no";
+    }
+}
