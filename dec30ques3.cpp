3be11a616960615a28d99053ff1fa4ae825e65ac
saumya singh <saumyasingh3234@gmail.com>
2025-12-30 16:24:21 +0530

    dec30ques3

---
+++ dec30ques3.cpp
@@ -0,0 +1,13 @@
+// https://www.codechef.com/problems/EXUNPIZZA
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,k,r;
+	cin>>n>>k>>r;
+	cout<<((n-k)*r);
+}
