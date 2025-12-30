f75d63a26c8c8a73dc3a8ec777a3dbc1abcaf22f
saumya singh <saumyasingh3234@gmail.com>
2025-12-30 16:20:04 +0530

    dec30ques2

--- dec30ques2.cpp
+++ dec30ques2.cpp
@@ -1,5 +1,17 @@
-// 
+// https://www.codechef.com/problems/INDDAY
 
 #include <iostream>
 #include <vector>
 #include <algorithm>
+using namespace std;
+
+int main() {
+    // your code goes here
+    int x;
+    cin >> x;
+    if (x > 15) {
+        cout << -1;
+    } else {
+        cout << (15 - x);
+    }
+}
