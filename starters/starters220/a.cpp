3becb1f85d9c3a0da16109bfd0e2060bad6fbfa8
saumya singh <saumyasingh3234@gmail.com>
2026-01-14 12:01:02 +0530

    added file

---
+++ starters/starters220/a.cpp
@@ -0,0 +1,15 @@
+// new year resolution
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    
+        int x;
+        cin >> x;
+        cout << x*31 << "\n";
+    
+    return 0;
+}
